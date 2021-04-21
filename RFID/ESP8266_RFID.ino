ffd3b9baf843ccfaf57d3493c4975bbda64c3d13
JSchwartz24 <jasonkobes24@gmail.com>
2021-04-21 12:25:16 -0700

    Update ESP8266_RFID.ino
    wifi capabilities added
    Http process started with, still needs to be personalized
    extra unnecessary code removed 
    comments added for readability

--- RFID/ESP8266_RFID.ino
+++ RFID/ESP8266_RFID.ino
@@ -1,6 +1,15 @@
+//RFID
 #include <SPI.h>
 #include <MFRC522.h>
 
+//WIFI
+#include <ESP8266WiFi.h>
+
+//HTTP
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+
+//RFID vars
 constexpr uint8_t RST_PIN = D3;     // Configurable, see typical pin layout above
 constexpr uint8_t SS_PIN = D4;     // Configurable, see typical pin layout above
 
@@ -8,8 +17,39 @@ MFRC522 rfid(SS_PIN, RST_PIN); // Instance of the class
 MFRC522::MIFARE_Key key;
 
 String tag;
+bool access = 0;// default to 0 or no access
+
+//WIFI vars
+const char* ssid     = "Da Frat";         // The SSID (name) of the Wi-Fi network you want to connect to
+const char* password = "DaFrat24";     // The password of the Wi-Fi network
+
+//HTTP
+String serverName = "http://192.168.1.106:1880/update-sensor";
+unsigned long lastTime = 0;
+unsigned long timerDelay = 5000;
 
 void setup() {
+  //WIFI establishment
+  Serial.begin(115200);         // Start the Serial communication to send messages to the computer
+  delay(10);
+  Serial.println('\n');
+  
+  WiFi.begin(ssid, password);             // Connect to the network
+  Serial.print("Connecting to ");
+  Serial.print(ssid); Serial.println(" ...");
+
+  int i = 0;
+  while (WiFi.status() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
+    delay(1000);
+    Serial.print(++i); Serial.print(' ');
+  }
+
+  Serial.println('\n');
+  Serial.println("Connection established!");  
+  Serial.print("IP address:\t");
+  Serial.println(WiFi.localIP());         // Send the IP address of the ESP8266 to the computer
+  
+  //RFID init
   Serial.begin(9600);
   SPI.begin(); // Init SPI bus
   rfid.PCD_Init(); // Init MFRC522
@@ -19,6 +59,7 @@ void setup() {
 }
 
 void loop() {
+  //RFID
   // Look for new cards
   if ( ! rfid.PICC_IsNewCardPresent())
     return;
@@ -48,39 +89,48 @@ void loop() {
   {
     Serial.println("Authorized access");
     Serial.println();
+    access = 1;
     delay(1000);
-
-    
-//    delay(500);
-//    digitalWrite(LED_G, HIGH);
-//    tone(BUZZER, 500);
-//    delay(300);
-//    noTone(BUZZER);
-//    myServo.write(180);
-//    delay(5000);
-//    myServo.write(0);
-//    digitalWrite(LED_G, LOW);
   }
  
  else   {
     Serial.println(" Access denied");
+    Serial.println();
+    access = 0;
     delay(1000);
+  }
+  
+  //HTTP
+  //Send an HTTP POST request every 10 minutes
+  if ((millis() - lastTime) > timerDelay) {
+    //Check WiFi connection status
+    if(WiFi.status()== WL_CONNECTED){
+      HTTPClient http;
 
-//    digitalWrite(LED_R, HIGH);
-//    tone(BUZZER, 300);
-//    delay(1000);
-//    digitalWrite(LED_R, LOW);
-//    noTone(BUZZER);
+      String serverPath = serverName + "?temperature=24.37";
+      
+      // Your Domain name with URL path or IP address with path
+      http.begin(serverPath.c_str());
+      
+      // Send HTTP GET request
+      int httpResponseCode = http.GET();
+      
+      if (httpResponseCode>0) {
+        Serial.print("HTTP Response code: ");
+        Serial.println(httpResponseCode);
+        String payload = http.getString();
+        Serial.println(payload);
+      }
+      else {
+        Serial.print("Error code: ");
+        Serial.println(httpResponseCode);
+      }
+      // Free resources
+      http.end();
+    }
+    else {
+      Serial.println("WiFi Disconnected");
+    }
+    lastTime = millis();
   }
 } 
-    
-//  if (rfid.PICC_ReadCardSerial()) {
-//    for (byte i = 0; i < 4; i++) {
-//      tag += rfid.uid.uidByte[i];
-//    }
-//    Serial.println(tag);
-//    tag = "";
-//    rfid.PICC_HaltA();
-//    rfid.PCD_StopCrypto1();
-//  }
-//} 
